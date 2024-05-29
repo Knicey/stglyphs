b40d2d84f721dad22b7475ab8db8fdcbcd44f334
Nathan Yang <nathan.s.yang@gmail.com>
2024-05-29 18:47:23 -0400

    Created stations.R and added todo tasks

--- data-raw/climate.R
+++ data-raw/climate.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(lubridate)
 library(janitor)
 
+
 #Near the Himalayas
 shiquanhe <- read_csv("data/NOAA/CHM00055228.csv")
 
@@ -30,20 +31,42 @@ dataset_cleaned <- whole_dataset |>
     year = year(date)
   ) |>
   filter(
-    year > 2018
+    year > 2017,
+    year != 2024
+  ) |>
+  select(
+
   )
 
-#The average here is calcaulted by taking the mean of the precipitation values for each month across all days in the past 5 years
+#The average here is calculated by taking the mean of the precipitation values for each month across all days in the past 5 years
 dataset_grouped <- dataset_cleaned |>
   group_by(month, name) |>
-  summarise(avg = mean(prcp, na.rm = TRUE))
+  summarise(
+    avgmin = mean(tmin, na.rm = TRUE) / 10,
+    avgmax = mean(tmax, na.rm = TRUE) / 10
+  )
 
-ggplot(data = dataset_grouped, mapping = aes(x = factor(month), y = avg, color = name, group = name)) +
-  geom_line() +
+ggplot(data = dataset_grouped) +
+  geom_segment(aes(x = factor(month), xend = factor(month), y = avgmin, yend = avgmax)) +
   theme_bw() +
   labs(
     title = "Average Monthly Precipitation Across Different Locations in the Past 5 Years",
     x = "Month",
     y = "Average Precipitation (in)",
     color = "Location"
-  )
+  ) +
+  facet_wrap(vars(name))
+
+
+#Use RNOAA package and put cleanup in stations.R
+#Choose the stations with more frequent reporting
+
+#Maps Package: SF
+#Create a basemap and put dots where the stations are
+
+#ggplot extension book (how a new geom is created), geoms (Chapters 18, 20(.3))
+#Look at ggrepel (2 geoms + position functions) package structure, documentation, etc.
+#Create GitHub workflow (package tests)
+
+#Look at the Glyph map paper to get an idea of the linear algebra
+#Use_data_raw()
---
+++ data-raw/stations.R
@@ -0,0 +1,5 @@
+## code to prepare `stations` dataset goes here
+
+
+
+usethis::use_data(stations, overwrite = TRUE)
