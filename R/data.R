0177fc18ffc549444a0ab6fa6b2102e469cd12f2
Nathan Yang <nathan.s.yang@gmail.com>
2024-06-09 23:21:44 -0400

    Fixed warnings with dataset documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' NOAA Weather Station Data
+#'
+#' A subset of data from NOAA weather stations in the United States.
+#' Selected stations include:
+#'
+#' @format ## `stations`
+#' A data frame with 17,253 rows and 12 columns:
+#' \describe{
+#'   \item{name}{Station Location}
+#'   \item{tmin, tmax}{The minimum and maximum temperatures}
+#'   \item{prcp}{Percepitation}
+#'   ...
+#' }
+#' @source <https://www.ncei.noaa.gov/access/search/data-search/daily-summaries>
+"stations"
