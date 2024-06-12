37025c31c64312fb6bec0821f12ed0a8924463ae
Nathan Yang <nathan.s.yang@gmail.com>
2024-06-12 17:46:05 -0400

    Fixing errors and adding experimental sf code

--- R/geom_segment_glyph.R
+++ R/geom_segment_glyph.R
@@ -2,7 +2,10 @@
 #' @import dplyr
 #' @export
 
-GeomPolygonHollow <- ggproto("GeomPolygonHollow", GeomPolygon,
+#' @title GeomSegmentGlyph
+#' @return description
+
+GeomSegmentGlyph <- ggproto("GeomSegmentGlyph", GeomPolygon,
                              default_aes = aes(
                                colour = "black",
                                fill = NA,
@@ -16,7 +19,7 @@ geom_chull <- function(mapping = NULL, data = NULL, stat = "chull",
                        position = "identity", na.rm = FALSE,
                        show.legend = NA, inherit.aes = TRUE, ...) {
   layer(
-    geom = GeomPolygonHollow,
+    geom = GeomSegmentGlyph,
     data = data,
     mapping = mapping,
     stat = stat,
---
+++ R/sf.R
@@ -0,0 +1,14 @@
+#library(sf)
+library(ggplot2)
+library(dplyr)
+#library(rnaturalearth)
+
+
+#us <- ne_states(country = "United States of America", returnclass = "sf")
+#mainland_us <- us %>%
+#  filter(!name %in% c("Alaska", "Hawaii", "Puerto Rico", "Guam", "American Samoa", "U.S. Virgin Islands", "Northern Mariana Islands"))
+
+
+#ggplot(mainland_us) +
+#  geom_sf() +
+#  geom_point(data = stations, aes(x = long, y = lat), color = "red", size = 6)
