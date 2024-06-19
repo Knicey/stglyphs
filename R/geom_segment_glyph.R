2d250b6bb7e8aca961de946e28336ef645793a4c
Nathan Yang <nathan.s.yang@gmail.com>
2024-06-18 21:22:56 -0400

    GeomSegmentGlyph aesthetics and data setup function

--- R/geom_segment_glyph.R
+++ R/geom_segment_glyph.R
@@ -5,27 +5,37 @@
 #' @title GeomSegmentGlyph
 #' @return description
 
-GeomSegmentGlyph <- ggproto("GeomSegmentGlyph", GeomSegment,
-                             default_aes = aes(
-                               colour = "black",
-                               fill = NA,
-                               linewidth = 0.5,
-                               linetype = 1,
-                               alpha = NA
-                             )
+GeomSegmentGlyph <- ggproto(
+  "GeomSegmentGlyph",
+  ggplot2::GeomSegment,
+
+  setup_data = function(data, params) {
+    data <- glyph_data_setup(data, params)
+  },
+
+  draw_panel = function(data, panel_params, coord, ...) {
+    ggplot2:::GeomPath$draw_panel(data, panel_params, coord, ...)
+  },
+
+
+  required_aes = c("x_major", "y_major", "x_minor", "y_minor", "yend_minor"),
+  default_aes = aes(
+    colour = "black",
+    linewidth = 0.5,
+    linetype = 1,
+    width = ggplot2::rel(2.1),
+    height = ggplot2::rel(2.1)
+
+  ),
+
 )
 
-geom_chull <- function(mapping = NULL, data = NULL, stat = "chull",
-                       position = "identity", na.rm = FALSE,
-                       show.legend = NA, inherit.aes = TRUE, ...) {
-  layer(
-    geom = GeomSegmentGlyph,
-    data = data,
-    mapping = mapping,
-    stat = stat,
-    position = position,
-    show.legend = show.legend,
-    inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
-  )
+
+glyph_data_setup <- function(data, params){
+  data <- data %>%
+    dplyr::mutate(
+      x_minor = x_scale(.data$x_minor)
+    )
+  data
 }
+
